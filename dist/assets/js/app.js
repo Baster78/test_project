a3be4a9754b9ca9c891f4d0f5afe083ffe28bea0
Baster78 <avror1g@yandex.ru>
2023-06-08 16:29:50 +0300

    last lesson and finish of MoGo

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -1,1 +1,32 @@
-console.log("file 1"),console.log("file 2");
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (function() { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/assets/js/app.js":
+/*!******************************!*\
+  !*** ./src/assets/js/app.js ***!
+  \******************************/
+/***/ (function() {
+
+eval("$(function () {\r\n  let header = $(\"#header\"),\r\n    introH = $(\"#intro\").innerHeight(),\r\n    scrollOffset = $(window).scrollTop();\r\n\r\n  // fixed header\r\n\r\n  checkScroll(scrollOffset);\r\n\r\n  $(window).on(\"scroll\", function () {\r\n    scrollOffset = $(this).scrollTop();\r\n\r\n    checkScroll(scrollOffset);\r\n  });\r\n\r\n  function checkScroll(scrollOffset) {\r\n    scrollOffset = $(this).scrollTop();\r\n\r\n    if (scrollOffset >= introH) {\r\n      header.addClass(\"fixed\");\r\n    } else {\r\n      header.removeClass(\"fixed\");\r\n    }\r\n  }\r\n\r\n  // smooth scroll\r\n\r\n  $(\"[data-scroll\").on(\"click\", function (event) {\r\n    event.preventDefault();\r\n\r\n    let $this = $(this),\r\n      blockId = $this.data(\"scroll\"),\r\n      blockOffset = $(blockId).offset().top;\r\n\r\n    $(\"#nav a\").removeClass(\"active\");\r\n    $this.addClass(\"active\");\r\n\r\n    $(\"html,body\").animate({ scrollTop: blockOffset }, 500);\r\n  });\r\n\r\n  // menu nav toggle\r\n\r\n  $(\"#nav__toggle\").on(\"click\", function (event) {\r\n    event.preventDefault();\r\n    $(this).toggleClass(\"active\");\r\n    $(\"#nav\").toggleClass(\"active\");\r\n  });\r\n});\r\n\n\n//# sourceURL=webpack://brainscloud/./src/assets/js/app.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/assets/js/app.js"]();
+/******/ 	
+/******/ })()
+;
--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,48 @@
-console.log('file 1');
+$(function () {
+  let header = $("#header"),
+    introH = $("#intro").innerHeight(),
+    scrollOffset = $(window).scrollTop();
+
+  // fixed header
+
+  checkScroll(scrollOffset);
+
+  $(window).on("scroll", function () {
+    scrollOffset = $(this).scrollTop();
+
+    checkScroll(scrollOffset);
+  });
+
+  function checkScroll(scrollOffset) {
+    scrollOffset = $(this).scrollTop();
+
+    if (scrollOffset >= introH) {
+      header.addClass("fixed");
+    } else {
+      header.removeClass("fixed");
+    }
+  }
+
+  // smooth scroll
+
+  $("[data-scroll").on("click", function (event) {
+    event.preventDefault();
+
+    let $this = $(this),
+      blockId = $this.data("scroll"),
+      blockOffset = $(blockId).offset().top;
+
+    $("#nav a").removeClass("active");
+    $this.addClass("active");
+
+    $("html,body").animate({ scrollTop: blockOffset }, 500);
+  });
+
+  // menu nav toggle
+
+  $("#nav__toggle").on("click", function (event) {
+    event.preventDefault();
+    $(this).toggleClass("active");
+    $("#nav").toggleClass("active");
+  });
+});
